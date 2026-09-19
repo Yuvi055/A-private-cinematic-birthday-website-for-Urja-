ebb78648572f629436b94a153b0fa1bf3572c44e
Yuvi055 <yogeshpraj004@gmail.com>
2026-09-19 07:51:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ document.getElementById("enterBtn").classList.remove("hidden");
 document.getElementById("enterBtn").style.display = "inline-block";
 
 document.getElementById("enterBtn").onclick = () => {
-  document.getElementById("intro").style.display = "none";
-  document.querySelector("header").style.display = "flex";
+document.getElementById("intro").classList.add("intro-hidden");  document.querySelector("header").style.display = "flex";
   document.querySelector("main").style.display = "block";
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
