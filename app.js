2dbcba2203393ad3d923a2b7e00c4e48be371821
Yuvi055 <yogeshpraj004@gmail.com>
2026-09-19 12:06:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ if (introVideo) {
   introVideo.volume = 1;
   introVideo.play().catch(() => {});
 }
-intro.classList.add("intro-hidden);
+intro.classList.add("intro-hidden");
     const header = document.querySelector("header");
     const main = document.querySelector("main");
 
